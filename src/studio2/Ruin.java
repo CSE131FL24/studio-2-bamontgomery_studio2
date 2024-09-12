0d1a8b6b34108885f2547474ba93c6e45ab14172
chasemontgomery <chasemontgomery@localhost>
2024-09-12 11:52:27 -0500

    Working on it

--- src/studio2/Ruin.java
+++ src/studio2/Ruin.java
@@ -1,9 +1,42 @@
 package studio2;
+import java.util.Scanner;
 
 public class Ruin {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Scanner in = new Scanner(System.in);
+		
+		
+		System.out.print("Please input start amount: ");
+		double startAmount = in.nextDouble();
+		System.out.print("Please input the win chance: ");
+		double winChance = in.nextDouble();
+		System.out.print("Please input the win limit: ");
+		double winLimit = in.nextDouble();
+		
+		double currentAmount = startAmount;
+		
+		while (currentAmount < winLimit) {
+			
+				
+			if (Math.random() > .5) {
+				
+				currentAmount++;
+				
+				System.out.println("");
+			}
+			
+			
+			
+			
+			
+			
+		}
+		
+		
+		
 
 	}
 
