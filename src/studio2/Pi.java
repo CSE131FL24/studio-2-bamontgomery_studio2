0716d0bad7480b582b7f7299d8ccf5625da15add
Ousmane Ba <baous2003@gmail.com>
2024-09-12 12:47:16 -0500

    Pi Class

--- src/studio2/Pi.java
+++ src/studio2/Pi.java
@@ -4,6 +4,26 @@ public class Pi {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+
+        int numThrows = 1000000;  
+        int dartsInCircle = 0;
+
+        for (int i = 0; i < numThrows; i++) {
+            
+            double x = Math.random();
+            double y = Math.random();
+
+            // Check if the dart is inside the quarter circle using x^2 + y^2 <= 1
+            double distanceFromOrigin = Math.sqrt(x * x + y * y);
+            if (distanceFromOrigin <= 1) {
+                dartsInCircle++;
+            }
+        }
+
+        // Pi is approximately 4 * (darts in circle / total darts)
+        double piEstimate = 4.0 * dartsInCircle / numThrows;
+        System.out.println("Estimated value of Pi: " + piEstimate);
 
 	}
 
